7e22b096354d99ad9867cdd1e1ac39691e825606
funnythingz <hiroki@hackleberry.me>
2013-09-25 23:32:17 +0900

    rm

--- client/header.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../packages/typescript-libs/node.d.ts" />
-/// <reference path="../packages/typescript-libs/meteor.d.ts" />
-
-Template['header']['title'] = function () {
-    return "densuke";
-}
--- test.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="./packages/typescript-libs/node.d.ts" />
-/// <reference path="./packages/typescript-libs/meteor.d.ts" />
-console.log("Typescript-compile of Meteor");
