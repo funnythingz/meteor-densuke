88a838b64394cf7a35fe1547967828f008a4ffa9
funnythingz <hiroki@hackleberry.me>
2013-09-25 18:06:35 +0900

    dir

--- client/densuke.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../reference.ts" />
-
-if (Meteor.isClient) {
-
-  Template['hello']['greeting'] = function () {
-    return "Typescript-compile of Meteor";
-  }
-
-}
---
+++ client/header.ts
@@ -0,0 +1,6 @@
+/// <reference path="../packages/typescript-libs/node.d.ts" />
+/// <reference path="../packages/typescript-libs/meteor.d.ts" />
+
+Template['header']['title'] = function () {
+    return "densuke";
+}
